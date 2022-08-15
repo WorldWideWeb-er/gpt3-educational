beb8bb93c5eaa59f9199733c0b9bd5b5ffe5ffe3
NateWeber02 <nate.weber.work@gmail.com>
2022-08-15 17:40:54 -0600

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//Insead of linking each compnent to the App.js,
+//This index.js is used to clean up the code and export each individual componenet onto one line
+export {default as Article} from './article/Article';
+export {default as Brand} from './brand/Brand';
+export {default as CTA} from './cta/CTA';
+export {default as Feature} from './feature/Feature';
+export {default as Navbar} from './navbar/Navbar';
---
+++ src/containers/index.jsx
@@ -0,0 +1,8 @@
+//Insead of linking each compnent to the App.js,
+//This index.js is used to clean up the code and export each individual componenet onto one line
+export {default as Blog} from './blog/Blog';
+export {default as Features} from './features/Features';
+export {default as Footer} from './footer/Footer';
+export {default as Header} from './header/Header';
+export {default as Possibility} from './possibility/Possibility';
+export {default as WhatGPT3} from './whatGPT3/WhatGPT3';
